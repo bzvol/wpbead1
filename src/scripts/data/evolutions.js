8512e6ad73f38c126a6c9d5088a21fd0e7d33e85
Benjamin Zvolenszki <zvolenszki.benjamin@gmail.com>
2025-04-12 19:04:29 +0200

    feat: use scripts as ES modules

--- src/scripts/data/evolutions.js
+++ src/scripts/data/evolutions.js
@@ -1,4 +1,4 @@
-const levels = {
+export const levels = {
     "easy": {
         "name": "Easy",
         "points": 5,
@@ -23,7 +23,7 @@ const levels = {
     }
 }
 
-const evolutions = [
+export const evolutions = [
     {
         "name": "Scripting Evolution",
         "tooltip": "Scripting.png",
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import {levels} from "./data/evolutions.js";
+
+// Generate options for the difficulty selector
 const difficultySelector = document.querySelector('#difficulty');
 Object.keys(levels).forEach(levelKey => {
     const option = document.createElement('option');
